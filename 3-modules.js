8a413f5e8020589fbd3167a1c602637c952d0e70
David <darakelyan5@gmail.com>
2022-12-04 20:53:06 -0500

    adding new file and modifying some

--- 3-modules.js
+++ 3-modules.js
@@ -1,14 +1,16 @@
 // Common JS, every file is now module (by default)
 // Modules - Encapsulated Code (only share Minimum)
+// When you import a module, you actually invoke it 
 
 const names = require('./4-namesModule');
 const sayHi = require('./5-utilsModule');
 const data = require('./6-alternative-flavor');
+require('./7-mind-grenade')
+
 // Module calls
 
-//console.log(names);
+console.log(names);
 sayHi('Howdy!')
 sayHi(names.peter)
 sayHi(names.john)
 console.log(data)
-
--- app.js
+++
@@ -1,8 +0,0 @@
-// Common JS, every file is now module (by default)
-// Modules - Encapsulated Code (only share Minimum)
-
-
-
-sayHi('susan')
-sayHi(peter)
-sayHi(peter)
