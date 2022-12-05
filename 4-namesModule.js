a56b624e9e156989fd2b0a2a3bd3175ee4ebfa43
eduard07438 <eduard@inncretech.com>
2022-12-04 20:27:36 -0500

    New lessons

---
+++ 4-namesModule.js
@@ -0,0 +1,8 @@
+// local
+const secret = 'SUPER SECRET'
+
+// shared, because it's part of the export
+const john = 'john'
+const peter = 'peter'
+
+module.exports = {john,peter}
--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-setInterval(() => {
-    console.log('hello world')
-},1000)
+// Common JS, every file is now module (by default)
+// Modules - Encapsulated Code (only share Minimum)
+
+
+
+sayHi('susan')
+sayHi(peter)
+sayHi(peter)
