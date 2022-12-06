c0170b1210d5dfa95b85928bd4722b507e2a1b74
David <darakelyan5@gmail.com>
2022-12-06 16:31:55 -0500

    adding fs async code

--- app.js
+++
@@ -1,8 +0,0 @@
-const {readFileSync,writeFileSync} = require('fs')
-
-const first = readFileSync('./content/first.txt','utf8')
-const second = readFileSync('./content/second.txt','utf8')
-
-console.log(first + second)
-
-writeFileSync('./content/result.txt',`Here is the result:\n${first}\n${second}`,{flag:'a'})
